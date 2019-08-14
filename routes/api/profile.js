2a3853276270fa8045bc767d27ee27d25416d4a2
utkarshm96 <umishra@mtu.edu>
2019-08-14 09:32:40 -0700

    final commit before css

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const request = require('request');
 const config = require('config');
 
@@ -151,7 +152,10 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo - remove users posts
+    //  remove users posts
+
+    await Post.deleteMany({ user: req.user.id });
+
     //remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // remove user
